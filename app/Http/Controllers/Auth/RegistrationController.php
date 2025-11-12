11848b1d288eaa2af35edba5303640223ad57397
Gintaras <gintaras.gaucys1@gmail.com>
2025-11-12 17:29:18 +0200

    fixed registration redirect

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -34,6 +34,6 @@ class RegistrationController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('home', absolute: false));
     }
 }
